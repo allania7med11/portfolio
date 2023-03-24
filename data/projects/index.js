7831a7de7b12b29af0b3634e65fede7b1da490ad
allania7med11 <allania7med11@gmail.com>
2023-03-24 12:58:08 +0000

    remove some profile projects

--- data/projects/index.js
+++ data/projects/index.js
@@ -1,18 +1,10 @@
 import travelPlanning from './travelPlanning.js';
-import naturalLanguageProcessing from './naturalLanguageProcessing.js';
-import zipweather from './zipweather.js';
 import bestElectricBike from './bestElectricBike.js';
 import discovertunisia from './discovertunisia.js';
-import calculator from './calculator.js';
 import structure from './structure.js';
-import legacystructure from './legacystructure.js';
 export default {
     travelPlanning,
-    naturalLanguageProcessing,
-    zipweather,
     bestElectricBike,
     discovertunisia,
-    calculator,
     structure,
-    legacystructure
 }
