0d11de924166351e8c10252f8d1ab86eee5e8906
allania7med11 <allania7med11@gmail.com>
2023-03-25 04:33:24 +0000

    add shoppingify features

--- data/projects/index.js
+++ data/projects/index.js
@@ -1,8 +1,10 @@
+import shoppingify from './shoppingify.js';
 import travelPlanning from './travelPlanning.js';
 import bestElectricBike from './bestElectricBike.js';
 import discovertunisia from './discovertunisia.js';
 import structure from './structure.js';
 export default {
+    shoppingify,
     travelPlanning,
     bestElectricBike,
     discovertunisia,
---
+++ data/projects/shoppingify.js
@@ -0,0 +1,43 @@
+export default {
+  name: "Shoppingify Webite",
+  technology: "Django/React",
+  image: "structure/entry.png",
+  technologies: ["Digital Ocean", "DRF", "Next", "Redux", "MUI"],
+  overview: `Shoppingify is an eCommerce website that offers a delightful experience for both shop owners and customers, thanks to its numerous features such as`,
+  features: [
+    "Product Management: Shop owners can easily add products with images, discounts, and categories to keep their store up-to-date and well-organized.",
+    "WYSIWYG Product Descriptions: Shop owners can add detailed product descriptions using a WYSIWYG editor, providing customers with the necessary information to make informed purchase decisions.",
+    "Category Navigation: Customers can navigate between different categories of products, making it easier to find what they're looking for.",
+    "Product Filtering: Customers can filter products by price, discount, or search text in the product name or description, allowing them to narrow down their options and find the best product for them.",
+    "Product Ordering: Customers can order products by name or price, making it easier to compare and select the products they want to purchase.",
+    "Detailed Product Information: Customers can review detailed product descriptions and multiple images before making a purchase, ensuring they have all the information they need to make an informed decision."
+  ],
+  images: [
+    {
+      src: require("@/assets/images/structure/home.png"),
+    },
+    {
+      src: require("@/assets/images/structure/login.png"),
+    },
+    {
+      src: require("@/assets/images/structure/section.png"),
+    },
+    {
+      src: require("@/assets/images/structure/detail.png"),
+    },
+    {
+      src: require("@/assets/images/structure/offline.png"),
+    },
+  ],
+  video: "https://www.youtube.com/embed/videoseries?list=PLDFGldFvahMtwgBPyknkKHGuu0pB1VMD4&amp;index=0",
+  actions: [
+    {
+      text: "Visit Website",
+      href: "https://structure.effectivewebapp.com/"
+    },
+    {
+      text: "View on Github",
+      href: "https://github.com/allania7med11/structure_back/"
+    }
+  ]
+};
