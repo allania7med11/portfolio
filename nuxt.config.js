9891731086d9cf21dd7631074601cc180933e216
allania7med11 <allania7med11@gmail.com>
2023-04-01 20:56:42 +0000

    update static directory

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,6 +5,9 @@ export default {
   /*
    ** Headers of the page
    */
+   generate: {
+    dir: "portfolio"
+  },
   head: {
     titleTemplate: "%s - " + process.env.npm_package_name,
     title: process.env.npm_package_name || "",
