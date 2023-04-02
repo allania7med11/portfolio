bf5ab9e0a68b6a14f7829df01cfb40fc2eb5000e
allania7med11 <allania7med11@gmail.com>
2023-04-02 02:00:51 +0000

    update skills in resume

--- data/skills.js
+++ data/skills.js
@@ -1,12 +1,14 @@
 export default [
   { name: "Html/Css", value: "90%" },
-  { name: "Javascript/Node/Express", value: "80%" },
+  { name: "Javascript/Typescript", value: "85%" },
   { name: "Python/Django/DRF", value: "90%" },
+  { name: "React/MATERIAL-UI/Next", value: "90%" },
+  { name: "Node/Express", value: "80%" },
   { name: "Vue/Vuetify/Nuxt", value: "85%" },
-  { name: "React/MATERIAL-UI", value: "85%" },
   { name: "Angular/Bootstrap", value: "70%" },
   { name: "SQL/Postgresql", value: "80%" },
   { name: "NoSQL/Mongodb", value: "80%" },
   { name: "Pytest/Cypress", value: "75%" },
   { name: "Photoshop/Filmora", value: "60%" },
+  { name: "Figma/UI", value: "75%" },
 ];
