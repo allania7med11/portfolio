6fd87104331ab18f00f39aa978623973add9b8f5
allania7med11 <allania7med11@gmail.com>
2025-07-20 19:34:26 +0100

    update ChatTag AI website url

--- data/projects/chattagai.js
+++ data/projects/chattagai.js
@@ -26,8 +26,8 @@ export default {
     ],
     actions: [
         {
-            text: "View Demo",
-            href: "https://chattagAi.effectivewebapp.com/"
+            text: "Visit Website",
+            href: "https://chattagai.com/"
         }
     ]
 };
